bc51b3551f5e6d2fea6e080ccd402e5fb17a84a6
Marek Romanowski <marek.romanowski@gmail.com>
2014-07-12 20:56:54 +0200

    Improving accounting visual side.

--- app/scripts/test/webapp.configuration.js
+++ app/scripts/test/webapp.configuration.js
@@ -1,6 +1,9 @@
 'use strict';
 
 angular.module('mt.webapp')
+    .run(function($rootScope) {
+      $rootScope.appName = 'Matsuo Core App';
+    })
     .config(function(userGroupConfigurationProvider) {
       userGroupConfigurationProvider.groupToDefaultRoute.push({ groupName: '', defaultRoute: '/base/webappPanel'});
     })
