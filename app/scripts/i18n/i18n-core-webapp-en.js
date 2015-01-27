5747bbb3e98ae2d79c5cfc4787bb3d38d077237b
Marek Romanowski <marek.romanowski@gmail.com>
2015-01-27 20:47:59 +0100

    Translations.

--- app/scripts/i18n/i18n-core-webapp-en.js
+++ app/scripts/i18n/i18n-core-webapp-en.js
@@ -4,5 +4,26 @@ angular.module('mt.webapp')
     .config(function($translateProvider) {
       // english translation
       $translateProvider.translations('en', {
+
+        dialog: {
+          addUser: {
+            newUserText: 'Add user'
+          }
+        },
+
+        entity: {
+          username: 'Login',
+          password: 'Pasword',
+          person: {
+            firstName: 'First name',
+            lastName: 'Last name',
+            pesel: 'ID number',
+            address: {
+              town: 'Town',
+              street: 'Street'
+            }
+          }
+        }
+
       });
     });
--- app/scripts/i18n/i18n-core-webapp-pl.js
+++ app/scripts/i18n/i18n-core-webapp-pl.js
@@ -4,5 +4,26 @@ angular.module('mt.webapp')
     .config(function($translateProvider) {
       // polish translation
       $translateProvider.translations('pl', {
+
+        dialog: {
+          addUser: {
+            newUserText: 'Nowy użytkownik'
+          }
+        },
+
+        entity: {
+          username: 'Login',
+          password: 'Hasło',
+          person: {
+            firstName: 'Imię',
+            lastName: 'Nazwisko',
+            pesel: 'Pesel',
+            address: {
+              town: 'Miasto',
+              street: 'Ulica'
+            }
+          }
+        }
+
       });
     });
